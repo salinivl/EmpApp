542de0b64f635e4eac6343f947789b6b21da1671
Salini <reachsalinivl@gmail.com>
2023-04-01 18:09:15 +0400

    upadated header

--- src/components/Empheader.jsx
+++ src/components/Empheader.jsx
@@ -6,7 +6,7 @@ const Empheader = () => {
     <div>
          <nav className="navbar navbar-expand-lg bg-body-secondary" >
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Employee App</a>
+    <Link to="/" className="navbar-brand" >Employee App</Link>
     <button className="navbar-toggler" type="button"  data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
